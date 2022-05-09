79c0c40dc5cdc5e9ebd06ad56ce5e73f339aa87a
CamilleCoustaury <93978788+CamilleCoustaury@users.noreply.github.com>
2022-05-09 14:21:04 +0200

    mise au propre

--- graph_generator.py
+++ graph_generator.py
@@ -14,51 +14,6 @@ from networkx.algorithms import bipartite
 
 ##############################################################################
 
-#cette fonction permet de générer un graph bipartite de base 
-
-def graph_generator(l,r,n):
-    #l = number of left edges on the bipartite graph (labelled from 0 to l-1)
-    #r = number of right edges on the bipartite graph (labelled from 0 to r-1)
-    #n = number of vertices 
-    vertices = []
-    for i in range(n):
-        vertices.append([rd.randint(0,l-1),rd.randint(0,r-1)])
-    return l,r,np.array(vertices)
-
-
-
-def utility(L):
-    #L = selected vertices for the alocation in the graph
-    return len(L)
-
-#this functions tells if a vertice is available or not to be added in the allocation
-#une arrête n'est pas dispo si son sommet de gauche est déjà utilisé par une autre arrete de l'allocation
-def available(allocation,new_vertice):
-    res = True
-    if (newvertice[0] in allocation[:,0]):
-        res = False
-    return res
-
-def new_node(l,k,nb_vertices):
-    #nb_vertices = nombre d'arretes entrant dans le noeuds de droite
-    #l = nombre de noeuds de gauche
-    #k = numéro du noeud de droite 
-    vertices = []
-    for i in range(k):
-        vertices.append([rd.randint(0,l-1),k])
-    return np.array([k,vertices])
-        
-
-def step_gready(allocation,new_node): 
-    vertices = new_node[1] #on récupère la liste d'arrêtes 
-    while len(vertices!=0):
-        to_add=rd.choice(new_node[1])# on choisit une arrete au hasard 
-        if available(to_add): # si elle est dispo on la choisit
-            return to_add
-        else:
-            vertices.remove(to_add) #sinon on la retire de la liste et on en choisi une autre
-    return 
-    
 
 def gready(d_U,d_V):
     #input :  two sequences5 of nonnegative integers
